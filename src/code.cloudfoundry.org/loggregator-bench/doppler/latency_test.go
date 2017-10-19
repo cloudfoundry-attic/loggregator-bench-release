f987f52e834248ee5237c95601efc605097078c7
Andrew Poydence <apoydence@pivotal.io>
2017-10-19 12:24:00 -0600

    Latency test writes less than the diode length
    Signed-off-by: Brady Love <blove@pivotal.io>

--- src/code.cloudfoundry.org/loggregator-bench/doppler/latency_test.go
+++ src/code.cloudfoundry.org/loggregator-bench/doppler/latency_test.go
@@ -6,22 +6,22 @@ import (
 	"testing"
 )
 
-func BenchmarkDopplerLatencyV1ToV1_1000(b *testing.B) {
-	log.Printf("Starting Doppler latency benchmark (V1ToV1_1000) (b.N = %d)...", b.N)
-	defer log.Printf("Done with Doppler latency benchmark (V1ToV1_1000) (b.N = %d).", b.N)
+func BenchmarkDopplerLatencyV1ToV1_100(b *testing.B) {
+	log.Printf("Starting Doppler latency benchmark (V1ToV1_100) (b.N = %d)...", b.N)
+	defer log.Printf("Done with Doppler latency benchmark (V1ToV1_100) (b.N = %d).", b.N)
 	producer := newV1Producer(grpcConfig)
 	consumer := newV1Consumer(grpcConfig)
 	defer producer.closeSend()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		for j := 0; j < 1000; j++ {
+		for j := 0; j < 100; j++ {
 			msg := fmt.Sprintf("message-%d", i)
 			payload := buildPayload([]byte(msg))
 			producer.send(payload)
 		}
 		// Assume Doppler drops some envelopes.
-		consumer.observe(500)
+		consumer.observe(80)
 	}
 	b.StopTimer()
 }
